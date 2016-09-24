8d88427bf69c8540adec569c4d4343f75af88760
AkashAkram <akashakram@outlook.com>
2016-09-24 20:04:22 +0600

    navbar color change

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -71,7 +71,7 @@
             </button>
 
             <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
+            <a class="navbar-brand height" href="{{ url('/') }}">
                 AK Mart
             </a>
         </div>
@@ -84,21 +84,22 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{ url('/add') }}">Add product</a></li>
-                <li><a href="{{ url('/cart') }}"><span class="fa fa-remove"></span>Cart</a></li>
+                
+                <li><a class="height" href="{{ url('/cart') }}"><span class="fa fa-remove"></span>Cart</a></li>
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
+                    <li><a class="height" href="{{ url('/login') }}">Login</a></li>
+                    <li><a class="height" href="{{ url('/register') }}">Register</a></li>
                 @else
+                <li><a class="height" href="{{ url('/add') }}">Add product</a></li>
                     <li class="">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        <a class="height" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
                             <li>
-                                <a href="{{ url('/logout') }}"
+                                <a  href="{{ url('/logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
                                     Logout
